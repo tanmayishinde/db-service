1079b9f3e62df966e8cd9a8dee538cc8c9242461
Tanmayi <tanmayi.shinde27@gmail.com>
2023-09-02 17:40:04 +0530

    Register&DeleteUser

--- src/main/java/com/root/dbservice/controllers/UserController.java
+++ src/main/java/com/root/dbservice/controllers/UserController.java
@@ -1,13 +1,12 @@
 package com.root.dbservice.controllers;
 
 import com.root.commondependencies.exception.ValidationException;
+import com.root.commondependencies.vo.DefaultResponseVO;
+import com.root.commondependencies.vo.DelRequestVO;
+import com.root.commondependencies.vo.UserVO;
 import com.root.dbservice.service.UserService;
-import com.root.dbservice.vo.UserVO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -18,6 +17,15 @@ public class UserController {
     @GetMapping("/getUserByEmail")
     public UserVO getUserByEmailId(@RequestParam("email") String email) throws ValidationException {
         return userService.getUserByEmail(email);
+    }
+
+    @PostMapping("/createUser")
+    public DefaultResponseVO createUser(@RequestBody UserVO requestVO){
+      return  userService.createUser(requestVO);
+    }
+    @DeleteMapping("/deleteUser")
+    public DefaultResponseVO deleteUser(@RequestBody DelRequestVO userVO){
+       return userService.deleteUser(userVO);
     }
 
 }
--- src/main/java/com/root/dbservice/repo/UserRepo.java
+++ src/main/java/com/root/dbservice/repo/UserRepo.java
@@ -1,6 +1,7 @@
 package com.root.dbservice.repo;
 
 import com.root.dbservice.entities.UserEntity;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,7 @@ import java.util.Optional;
 public interface UserRepo extends JpaRepository<UserEntity, Long> {
 
     Optional<UserEntity> findByEmail(String email);
+    @Transactional
+    void deleteUserByEmail(String email);
 
 }
